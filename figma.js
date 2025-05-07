9ec783fc64bfd978eca30fced3b79a60eef5980f
Irsha-fatima <iirshafatima@gmail.com>
2025-05-07 15:44:50 +0500

    Add full project files with responsive layout

--- figma.js
+++
@@ -1,7 +0,0 @@
-let icon = document.querySelector(".icon");
-let ul = document.querySelector("ul");
-
-icon.addEventListener("click", ()=>{
-  ul.classList.toggle("showData");
-  console.log(ul);
-})
